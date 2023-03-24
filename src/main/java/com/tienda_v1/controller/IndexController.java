a0fb9795068220e53792122ab78e63fe5bee6df0
PrisMarvelDC <101735415+PrisMarvelDC@users.noreply.github.com>
2023-03-24 11:21:19 -0600

    Semana 10

--- src/main/java/com/tienda_v1/controller/IndexController.java
+++ src/main/java/com/tienda_v1/controller/IndexController.java
@@ -1,22 +1,25 @@
 
 package com.tienda_v1.controller;
 
-import com.tienda_v1.domain.Cliente;
-import com.tienda_v1.service.ClienteService;
-import java.util.Arrays;
+import com.tienda_v1.service.ArticuloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
     
+     @Autowired
+    private ArticuloService articuloService;
+
+    
   
     @GetMapping("/")
     public String inicio(Model model){
-   
+     var articulos = articuloService.getArticulos(false);
+      model.addAttribute("articulos", articulos);
+        
         return "index";
        
     }
